678b9de1751f9b3c678a10dd4e1ffa09e787c702
root <root@localhost.localdomain>
2021-12-18 15:04:00 +0700

    v0.1.2-alpha1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React, {PureComponent} from "react";
 import Config from "../configs/Config";
 import {db} from "../configs/FirebaseConfig";
-import {collection, getDocs, limit, onSnapshot, orderBy, query, startAt, startAfter, Timestamp, where} from "firebase/firestore";
+import {collection, doc, getDoc, getDocs, limit, onSnapshot, orderBy, query, setDoc, startAt, startAfter, Timestamp, where} from "firebase/firestore";
 import IsEmpty from "../helpers/IsEmpty";
 import update from "immutability-helper";
 import LinearProgress from "@mui/material/LinearProgress";
@@ -15,9 +15,9 @@ import LoadingButton from "@mui/lab/LoadingButton";
 import {Modal} from "react-bootstrap";
 import Template from "../template/Template";
 import BgForest from "../helpers/backgrounds/BgForest";
-import "../css/Home.css";
 import Web3Context from "../contexts/Web3Context";
 import ErrorNotDeployed from "../helpers/errors/ErrorNotDeployed";
+import "../css/Home.css";
 
 class Home extends PureComponent {
     constructor(props) {
@@ -29,6 +29,7 @@ class Home extends PureComponent {
         const page = parseInt(params.get("page")) || 1;
         this.thirtyMinutesInMillis = 1000 * 60 * 30;
         this.statuses = {
+            zero: "not_deployed",
             first: "sleep",
             second: "awake",
             third: "done",
@@ -39,7 +40,9 @@ class Home extends PureComponent {
             limit: limit(perPage),
             listener: null,
             loading: false,
+            canSeeOutput: false,
             modalDetail: false,
+            modalAdd: false,
             data: [],
             query: {
                 search: search,
@@ -72,6 +75,18 @@ class Home extends PureComponent {
                     seconds: 0,
                     nanoseconds: 0
                 }
+            },
+            addNew: {
+                domain: "",
+                start_crunch: "",
+                end_crunch: "",
+                last_crunch: ""
+            },
+            addNewError: {
+                domain: false,
+                start_crunch: false,
+                end_crunch: false,
+                last_crunch: false
             }
         };
     }
@@ -95,7 +110,9 @@ class Home extends PureComponent {
     }
 
     getStatusText(data) {
-        if (data.output.length > 0 && this.isSleep(data)) {
+        if (data.status === this.statuses.zero) {
+            return <p className="m-0 fw-bold"><span className="text-warning">Not Deployed</span></p>;
+        } else if (data.output.length > 0 && this.isSleep(data)) {
             return <p className="m-0 fw-bold"><span className="text-primary">Found</span> & <span className="text-danger">Sleep</span></p>;
         } else if (data.output.length > 0 && data.status === this.statuses.second) {
             return <p className="m-0 fw-bold"><span className="text-primary">Found</span> & <span className="text-info">Awake</span></p>;
@@ -116,39 +133,134 @@ class Home extends PureComponent {
         }
     }
 
-    awake(data) {
-        this.setState({
-            loading: true
-        }, () => {
-            if (!IsEmpty(this.context.kaytrin)) {
-                this.context.kaytrin.deployed().then((contract) => {
-                    contract.exec.call({
-                        from: this.context.account
-                    }).then((result) => {
-                        if (result) {
-                            let goto = document.createElement("a");
-                            goto.href = `https://${data.domain}.herokuapp.com/pvk?query=live.js+-f+checking-addresses.txt+-s+${data.last_crunch}+-e+${data.end_crunch}`;
-                            goto.target = "_blank";
-                            goto.click();
-                            goto.remove();
-                        }
+    verifyOwner() {
+        return new Promise((resolve, reject) => {
+            this.setState({
+                loading: true
+            }, () => {
+                if (!IsEmpty(this.context.kaytrin)) {
+                    this.context.kaytrin.deployed().then((contract) => {
+                        contract.exec.call({
+                            from: this.context.account
+                        }).then((result) => {
+                            if (result) resolve(result);
+                        }).catch((error) => {
+                            window.alert("You're not the owner!");
+                        }).finally(() => {
+                            this.setState({
+                                loading: false
+                            });
+                        });
                     }).catch((error) => {
-                        window.alert("You're not the owner!");
-                    }).finally(() => {
                         this.setState({
                             loading: false
+                        }, () => {
+                            window.alert("Wrong network, use Ropsten Network!");
+                            ErrorNotDeployed(this.context.kaytrin, error);
                         });
-                    });
-                }).catch((error) => {
+                    }).finally(() => {});
+                } else {
                     this.setState({
                         loading: false
                     }, () => {
-                        window.alert("Wrong network, use Ropsten Network!");
-                        ErrorNotDeployed(this.context.kaytrin, error);
+                        window.alert("You're not connected to Blockchain!");
                     });
-                }).finally(() => {});
+                }
+            });
+        });
+    }
+
+    seeOutput() {
+        this.verifyOwner().then((result) => {
+            this.setState({
+                canSeeOutput: true
+            }, () => {
+                setTimeout(() => {
+                    this.setState({
+                        canSeeOutput: false
+                    });
+                }, 5000);
+            });
+        });
+    }
+
+    awake(data) {
+        this.verifyOwner().then((result) => {
+            let goto = document.createElement("a");
+            goto.href = `https://${data.domain}.herokuapp.com/pvk?query=live.js+-f+checking-addresses.txt+-s+${data.last_crunch}+-e+${data.end_crunch}`;
+            goto.target = "_blank";
+            goto.click();
+            goto.remove();
+        });
+    }
+
+    addNew() {
+        this.setState({
+            addNewError: {
+                domain: false,
+                start_crunch: false,
+                end_crunch: false,
+                last_crunch: false
+            }
+        }, () => {
+            let errorDomain = false;
+            let errorStartCrunch = false;
+            let errorEndCrunch = false;
+            let errorLastCrunch = false;
+            if (IsEmpty(this.state.addNew.domain)) errorDomain = true;
+            if (IsEmpty(this.state.addNew.start_crunch)) errorStartCrunch = true;
+            if (IsEmpty(this.state.addNew.end_crunch)) errorEndCrunch = true;
+            if (IsEmpty(this.state.addNew.last_crunch)) errorLastCrunch = true;
+            if (!errorDomain && !errorStartCrunch && !errorEndCrunch && !errorLastCrunch) {
+                this.verifyOwner().then((result) => {
+                    getDoc(doc(db, "pvks", this.state.addNew.domain)).then((data) => {
+                        if (IsEmpty(data.data())) {
+                            setDoc(doc(db, "pvks", this.state.addNew.domain), {
+                                id: this.state.lastData.id + 1,
+                                domain: this.state.addNew.domain,
+                                start_crunch: this.state.addNew.start_crunch,
+                                end_crunch: this.state.addNew.end_crunch,
+                                last_crunch: this.state.addNew.last_crunch,
+                                output: [],
+                                status: this.statuses.zero,
+                                updated_at: Timestamp.now()
+                            }).then((data) => {
+                                this.setState({
+                                    modalAdd: false
+                                }, () => {
+                                    this.props.navigate(Config.Links.Home);
+                                });
+                            }).catch((error) => {
+                                window.alert("Whoops, something went wrong!");
+                            }).finally(() => {
+                                this.setState({
+                                    loading: false
+                                });
+                            });
+                        } else {
+                            this.setState({
+                                loading: false
+                            }, () => {
+                                window.alert("Domain already exists!");
+                            });
+                        }
+                    }).catch((error) => {
+                        this.setState({
+                            loading: false
+                        }, () => {
+                            window.alert("Whoops, something went wrong!");
+                        });
+                    }).finally(() => {});
+                });
             } else {
-                window.alert("You're not connected to Blockchain!");
+                this.setState({
+                    addNewError: {
+                        domain: errorDomain,
+                        start_crunch: errorStartCrunch,
+                        end_crunch: errorEndCrunch,
+                        last_crunch: errorLastCrunch
+                    }
+                });
             }
         });
     }
@@ -357,10 +469,19 @@ class Home extends PureComponent {
                                     <td valign="top" className="p-0">
                                         <p className="m-0 px-1">:</p>
                                     </td>
-                                    <td valign="middle" className="p-0">
-                                        {this.state.dataDetail.output.length > 0 ? this.state.dataDetail.output.map((value, index) => (
-                                            <p key={index} className="m-0">{value}</p>
-                                        )) : <p className="m-0">-</p>}
+                                    <td valign="top" className="p-0">
+                                        {this.state.canSeeOutput ? <>
+                                            {this.state.dataDetail.output.length > 0 ? this.state.dataDetail.output.map((value, index) => (
+                                                <p key={index} className="m-0">{value}</p>
+                                            )) : <p className="m-0">-</p>}
+                                        </> : <LoadingButton
+                                            size={"small"}
+                                            color={"success"}
+                                            loading={this.state.loading}
+                                            variant={"outlined"}
+                                            className="p-0"
+                                            onClick={(event) => this.seeOutput()}
+                                        >See</LoadingButton>}
                                     </td>
                                 </tr>
                                 </tbody>
@@ -371,12 +492,70 @@ class Home extends PureComponent {
                         </div>
                     </Modal.Body>
                 </Modal>
+                <Modal size={"xl"} show={this.state.modalAdd} onHide={() => this.setValue("modalAdd", false)}>
+                    <Modal.Body className="data-detail">
+                        <h6 className="m-0">Add New Domain</h6>
+                        <div className="mt-3">
+                            <TextField
+                                label="Domain *"
+                                size={"small"}
+                                className="w-100"
+                                error={this.state.addNewError.domain}
+                                value={this.state.addNew.domain}
+                                onChange={(event) => this.setValue("addNew", update(this.state.addNew, {
+                                    domain: {$set: event.target.value}
+                                }))}
+                            />
+                            <TextField
+                                label="Start Crunch *"
+                                size={"small"}
+                                className="w-100 mt-3"
+                                error={this.state.addNewError.start_crunch}
+                                value={this.state.addNew.start_crunch}
+                                onChange={(event) => this.setValue("addNew", update(this.state.addNew, {
+                                    start_crunch: {$set: event.target.value}
+                                }))}
+                            />
+                            <TextField
+                                label="End Crunch *"
+                                size={"small"}
+                                className="w-100 mt-3"
+                                error={this.state.addNewError.end_crunch}
+                                value={this.state.addNew.end_crunch}
+                                onChange={(event) => this.setValue("addNew", update(this.state.addNew, {
+                                    end_crunch: {$set: event.target.value}
+                                }))}
+                            />
+                            <TextField
+                                label="Last Crunch *"
+                                size={"small"}
+                                className="w-100 mt-3"
+                                error={this.state.addNewError.last_crunch}
+                                value={this.state.addNew.last_crunch}
+                                onChange={(event) => this.setValue("addNew", update(this.state.addNew, {
+                                    last_crunch: {$set: event.target.value}
+                                }))}
+                            />
+                        </div>
+                        <div className="text-center mt-5">
+                            <button className="btn btn-sm text-white bgc-1F1E30 px-4" onClick={(event) => this.setValue("modalAdd", false)}>Close</button>
+                            <LoadingButton
+                                size={"small"}
+                                color={"primary"}
+                                loading={this.state.loading}
+                                variant={"contained"}
+                                onClick={(event) => this.addNew()}
+                                className="ms-2 px-4"
+                            >Add</LoadingButton>
+                        </div>
+                    </Modal.Body>
+                </Modal>
                 <BgForest className="app-content">
                     {this.state.loading && <LinearProgress />}
                     <div className="py-3">
                         <div className="container">
                             <div className="rounded d-flex justify-content-between box-shadow-primary bgc-white-opacity-95 hc-pagination px-3 py-2">
-                                <FormControl size={"small"} sx={{minWidth: 80}}>
+                                <FormControl size={"small"} sx={{minWidth: 100}}>
                                     <InputLabel id="query-filter">Filter</InputLabel>
                                     <Select
                                         labelId="query-filter"
@@ -388,6 +567,7 @@ class Home extends PureComponent {
                                         autoWidth
                                     >
                                         <MenuItem value=""><em>None</em></MenuItem>
+                                        <MenuItem value={this.statuses.zero}>Not Deployed</MenuItem>
                                         <MenuItem value={this.statuses.first}>Sleep</MenuItem>
                                         <MenuItem value={this.statuses.second}>Awake</MenuItem>
                                         <MenuItem value={this.statuses.third}>Done</MenuItem>
@@ -410,6 +590,12 @@ class Home extends PureComponent {
                                     variant={"outlined"}
                                     onClick={(event) => this.onPageChange(event, 1)}
                                 >Search</LoadingButton>
+                                <LoadingButton
+                                    color={"primary"}
+                                    variant={"contained"}
+                                    onClick={(event) => this.setValue("modalAdd", true)}
+                                    className="ms-2 white-space-nowrap"
+                                >Add New</LoadingButton>
                             </div>
                             <div className="row">
                                 {this.state.data.map((value, index, array) => (
